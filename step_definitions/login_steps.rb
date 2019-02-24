984ef7f8f3486c7a50e2e2f0df531cba6d784d3a
Vuk Mileusnić <vuk.vule@gmail.com>
2019-02-24 14:08:39 +0100

    Update login_steps.rb

--- step_definitions/login_steps.rb
+++ step_definitions/login_steps.rb
@@ -1,1 +1,16 @@
 
+Given("I open login page of humanity")  do
+  visit(LoginPage)
+end
+
+When(/^I enter username "([^"]*)"$/) do |login|
+  on(LoginPage).login_name = login
+end
+
+And(/^I enter password "([^"]*)"$/) do |password|
+  on(LoginPage).password = password
+end
+
+When("I press Login") do
+  on(LoginPage).login
+end
